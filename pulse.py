0e28ae7b971fa287a4a5f5313543eaeee59cbd4b
Matthew Stiller <mstiller@ggc.edu>
2020-12-15 21:26:33 -0500

    Debug tweaks. Defer to JS?

--- pulse.py
+++ pulse.py
@@ -1,4 +1,5 @@
 import re
+import requests
 import sys
 import zipfile
 
@@ -6,16 +7,21 @@ import zipfile
 
 for filename in sys.argv[1:]:
     z = zipfile.ZipFile(open(filename, 'rb'))
-    contents = (z.read("META-INF/mods.toml")).replace(" ","")
+    contents = (z.read("META-INF/mods.toml")).replace(" ", "")
     # print(contents)
-    
+
     regex = r"^displayURL\S+"
     matches = re.search(regex, contents, re.MULTILINE)
-    
+
     try:
-      url = matches.group(0).partition("=")[2].replace("\"", '')
+
+        url = matches.group(0).partition("=")[2].replace("\"", '')
+
+        
+        r = requests.get(url)
+        print(r.content.decode())
     except AttributeError:
-      url = "Update URL not present or invalid."
-      # print(contents)
+        url = "Update URL not present or invalid."
+        # print(contents)
     print(url)
     z.close()
