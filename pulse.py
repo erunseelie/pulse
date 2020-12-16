486f2c0d360f894eb952fb602cdc5127724a93d4
Matthew Stiller <mstiller@ggc.edu>
2020-12-15 20:28:41 -0500

    Basic structure. Gets specific mods.toml URL.

---
+++ pulse.py
@@ -0,0 +1,21 @@
+import re
+import sys
+import zipfile
+
+# first, one file POC
+
+for filename in sys.argv[1:]:
+    z = zipfile.ZipFile(open(filename, 'rb'))
+    contents = (z.read("META-INF/mods.toml")).replace(" ","")
+    # print(contents)
+    
+    regex = r"^displayURL\S+"
+    matches = re.search(regex, contents, re.MULTILINE)
+    
+    try:
+      url = matches.group(0).partition("=")[2].replace("\"", '')
+    except AttributeError:
+      url = "Update URL not present or invalid."
+      # print(contents)
+    print(url)
+    z.close()
